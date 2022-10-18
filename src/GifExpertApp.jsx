4baf1254fd363a102820ea748699f8afb3c23645
VeronicaKR <veronicamatarinmartinez@gmail.com>
2022-10-18 10:01:32 +0200

    final app gifs

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,12 +1,16 @@
 import { useState } from "react";
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 
 export const GifExpertApp = () => {
 
-  const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+  const [categories, setCategories] = useState(['One Punch']);
   
   const onAddCategory = (newCategory) => {
+
+    if ( categories.includes(newCategory)) return;
+
     setCategories([newCategory,...categories])
     //console.log(newCategory)
   }
@@ -19,13 +23,12 @@ export const GifExpertApp = () => {
     onNewCategory = {onAddCategory}
 
     />
-    <ol>
-      {categories.map(category =>{
-      return <li key={category}>{category}</li>
+     {
+      categories.map((category) => (
+      <GifGrid key={category} category= {category}/>))
       }
-      )}
       
-    </ol>
+    
     
     </>
   )
